205b45aa97d2b6f53fa9542b9dcefdd05e18588d
theoreticsinc <theoreticsinc@gmail.com>
2015-10-25 11:46:54 +0800

    *For My School Buddy Demos

---
+++ app/src/main/java/com/theoreticsinc/myschoolbuddyapp/models/NewsletterPage.java
@@ -0,0 +1,12 @@
+package com.theoreticsinc.myschoolbuddyapp.models;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Angelo on 9/30/2015.
+ */
+public class NewsletterPage {
+
+    @SerializedName("newsletters")
+    public NewsletterModel newsletters;
+}
